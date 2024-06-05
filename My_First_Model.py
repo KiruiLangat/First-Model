b37e413ec9541a6e8178a94e32fe80ba72d7acea
KiruiLangat <ted.kirui@students.ku.ac.ke>
2024-06-05 17:54:17 +0300

    Data Change

--- My_First_Model.py
+++ My_First_Model.py
@@ -10,24 +10,31 @@ iowa_file_path = 'C:/Users/sms20/Machine Learning/First-Model/melb_data.csv'
 
 home_data = pd.read_csv(iowa_file_path)
 # Create target object and call it y
-y = home_data.SalePrice
-# Create X
-features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X = home_data[features]
 
-# Split into validation and training data
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+print(home_data.columns)
+print(home_data.head())
+home_data.dropna(axis=0)
 
-# Specify Model
-iowa_model = DecisionTreeRegressor(random_state=1)
-# Fit Model
-iowa_model.fit(train_X, train_y)
+# y = home_data.Price
 
-# Make validation predictions and calculate mean absolute error
-val_predictions = iowa_model.predict(val_X)
-val_mae = mean_absolute_error(val_predictions, val_y)
-print("Validation MAE: {:,.0f}".format(val_mae))
+# # Create X
+# features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+# X = home_data[features]
 
-# Set up code checking
-binder.bind(globals())
-print("\nSetup complete")
+# # Split into validation and training data
+# train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# # Specify Model
+# iowa_model = DecisionTreeRegressor(random_state=1)
+
+# # Fit Model
+# iowa_model.fit(train_X, train_y)
+
+# # Make validation predictions and calculate mean absolute error
+# val_predictions = iowa_model.predict(val_X)
+# val_mae = mean_absolute_error(val_predictions, val_y)
+# print("Validation MAE: {:,.0f}".format(val_mae))
+
+# # Set up code checking
+# binder.bind(globals())
+# print("\nSetup complete")
