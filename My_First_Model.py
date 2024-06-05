c5acdee8328a5982f312cc4d4a81dee191c09179
KiruiLangat <ted.kirui@students.ku.ac.ke>
2024-06-05 18:29:22 +0300

    Optimizing decision trees

--- My_First_Model.py
+++ My_First_Model.py
@@ -39,3 +39,23 @@ print("Validation MAE: {:,.0f}".format(val_mae))
 # Set up code checking
 binder.bind(globals())
 print("\nSetup complete")
+
+# function 
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# Loop to compare tree sizes
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+best_tree_size = min(scores, key=scores.get)
+print(best_tree_size)
+
+# Fit the model with best_tree_size. Fill in argument to make optimal size
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=1)
+
+# fit the final model
+final_model.fit(X, y)
